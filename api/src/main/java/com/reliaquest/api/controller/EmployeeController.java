396829a372f24b4ea6183fbdf3a4154f041b8293
Rajat <rajat.dhanotiya@pubmatic.com>
2025-01-19 21:03:33 +0530

    update create employee response status code

--- api/src/main/java/com/reliaquest/api/controller/EmployeeController.java
+++ api/src/main/java/com/reliaquest/api/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.reliaquest.api.dto.CreateEmployeeDto;
 import com.reliaquest.api.entities.Employee;
 import com.reliaquest.api.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,7 @@ public class EmployeeController implements IEmployeeController{
 
     @Override
     public ResponseEntity<Employee> createEmployee(CreateEmployeeDto employeeInput) throws IOException{
-        return ResponseEntity.ok(employeeService.createEmployee(employeeInput));
+        return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.createEmployee(employeeInput));
     }
 
     @Override
